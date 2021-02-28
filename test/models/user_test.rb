178d1703fcd050789b16e3f7d725aa940bb06998
Ammy_Varela <ammyvrodriguez@hotmail.com>
2021-02-28 13:55:37 +0100

    minor test added

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,5 +4,7 @@ class UserTest < ActiveSupport::TestCase
   test "full_name returns the capitalize first_name and last_name" do
     user = User.new(first_name: "ringo", last_name: "star")
     assert_equal "Ringo Star", user.full_name
+    user = User.new(first_name: "john", last_name: "Lenon")
+    assert_equal "Ringo Star", user.full_name
   end
 end
