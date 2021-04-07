e8128822177aaf97c483074c1c0c98d5b2e1652c
Ammy_Varela <ammyvrodriguez@hotmail.com>
2021-04-07 22:33:13 +0200

    added updated seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,14 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
 
+pictures = ['https://images.pexels.com/photos/170811/pexels-photo-170811.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500', 'https://images.pexels.com/photos/210019/pexels-photo-210019.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500', 'https://images.pexels.com/photos/1149137/pexels-photo-1149137.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500', 'https://images.pexels.com/photos/707046/pexels-photo-707046.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500', 'https://images.pexels.com/photos/1592384/pexels-photo-1592384.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500']
+
 puts 'Creating 5 random products...'
 5.times do |i|
   product = Product.create!(
-    name: Faker::Company.name,
-    tagline: Faker::Company.catch_phrase
+    name: Faker::Coffee.blend_name,
+    tagline: Faker::Company.catch_phrase,
+    url: pictures.sample
   )
   puts "#{i + 1}. #{product.name}"
 end
