990d9e3acda6ea48e39a122afd1901ddf3db22e2
Ammy_Varela <ammyvrodriguez@hotmail.com>
2021-04-09 14:20:17 +0200

    added second test for creating new product

--- test/system/products_test.rb
+++ test/system/products_test.rb
@@ -6,4 +6,14 @@ class ProductsTest < ApplicationSystemTestCase
     assert_selector "h1", text: "Find here all products"
     assert_selector ".card-product", count: Product.count
   end
+
+  test "a signed in user must be able to create a product" do
+    login_as users(:george)
+    visit "products/new"
+    fill_in "product_name", with: "White Mocca"
+    fill_in "product_tagline", with: "Brand new model brought from Canada"
+    click_on "Create product"
+    assert_equal root_path, page.current_path
+    assert_text "Brand new model brought from Canada"
+  end
 end
