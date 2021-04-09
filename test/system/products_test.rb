8477fa148735875ac234ac1993b59df7bed55357
Ammy_Varela <ammyvrodriguez@hotmail.com>
2021-04-09 14:09:05 +0200

    added first test for home

--- test/system/products_test.rb
+++ test/system/products_test.rb
@@ -3,7 +3,7 @@ require "application_system_test_case"
 class ProductsTest < ApplicationSystemTestCase
   test "visiting the index" do
     visit root_url
-
     assert_selector "h1", text: "Find here all products"
+    assert_selector ".card-product", count: Product.count
   end
 end
