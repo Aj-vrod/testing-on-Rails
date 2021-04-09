0e7a96396679380b4b6044761370b0f6b4f6ede6
Ammy_Varela <ammyvrodriguez@hotmail.com>
2021-04-09 14:26:32 +0200

    added new field in test for creating a product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,5 +11,9 @@ class ProductsController < ApplicationController
   def create
   end
 
+  private
 
+  def product_params
+    params.require(:product).permit(:name, :tagline, :url)
+  end
 end
--- test/system/products_test.rb
+++ test/system/products_test.rb
@@ -12,6 +12,7 @@ class ProductsTest < ApplicationSystemTestCase
     visit "products/new"
     fill_in "product_name", with: "White Mocca"
     fill_in "product_tagline", with: "Brand new model brought from Canada"
+    fill_in "product_url", with: "https://images.pexels.com/photos/2365572/pexels-photo-2365572.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
     click_on "Create product"
     assert_equal root_path, page.current_path
     assert_text "Brand new model brought from Canada"
